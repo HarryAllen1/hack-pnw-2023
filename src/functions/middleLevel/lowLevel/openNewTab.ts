22e33834a249ba31138ed19811cad928186dfdec
lsakunes <senukasl@outlook.com>
2023-02-04 14:05:22 -0800

    changed openTab

--- src/functions/middleLevel/lowLevel/openNewTab.ts
+++ src/functions/middleLevel/lowLevel/openNewTab.ts
@@ -1,5 +1,13 @@
 let defaulturl = "https://www.google.com";
 
-export default function openNewTab(url = defaulturl) { 
-    window.open(url, "_blank");
+export default function openNewTab(url = defaulturl, incognito = false, newWindow = false, focus = true) {
+    if (incognito) {
+        url = "chrome://incognito/" + url;
+    }
+    if (newWindow) {
+        chrome.windows.create({ url: url, focused: focus });
+    }
+    else {
+        chrome.tabs.create({ url: url, active: focus });
+    }
 }
