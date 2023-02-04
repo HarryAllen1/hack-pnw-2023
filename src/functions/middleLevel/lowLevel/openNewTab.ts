294e3b7632da202cadfd9420a14950ed70713fbe
lsakunes <senukasl@outlook.com>
2023-02-04 12:09:35 -0800

    fixed a small thing

--- src/functions/middleLevel/lowLevel/openNewTab.ts
+++ src/functions/middleLevel/lowLevel/openNewTab.ts
@@ -1,5 +1,5 @@
 let defaulturl = "https://www.google.com";
 
-export default function openNewTab(url: string) {
-    window.open(url ? url : defaulturl, "_blank");
+export default function openNewTab(url = defaulturl) { 
+    window.open(url, "_blank");
 }
