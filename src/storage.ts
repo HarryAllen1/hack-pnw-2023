68d664664939419f2e48943730808df39f5e8820
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-04 13:47:50 -0800

    asdf

--- src/storage.ts
+++ src/storage.ts
@@ -1,16 +1,18 @@
 // the chrome namespace doesn't exist in the browser
 
-interface Command {
+export interface Command {
   name: string;
   code: string;
 }
 
 export const getCommands = async (): Promise<Command[]> => {
-  if (chrome) return (await chrome.storage.sync.get('commands')).commands ?? [];
+  if (typeof chrome !== 'undefined' && 'storage' in chrome)
+    return (await chrome.storage.sync.get('commands')).commands ?? [];
   else return JSON.parse(localStorage.getItem('commands') ?? '[]');
 };
 
 export const setCommands = async (commands: Command[]) => {
-  if (chrome) await chrome.storage.sync.set({ commands });
+  if (typeof chrome !== 'undefined' && 'storage' in chrome)
+    await chrome.storage.sync.set({ commands });
   else localStorage.setItem('commands', JSON.stringify(commands));
 };
