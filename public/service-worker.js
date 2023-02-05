bbe1bbf7ad1241de2b11a8254aebd438dac73d79
lsakunes <senukasl@outlook.com>
2023-02-04 18:55:26 -0800

    th

--- public/service-worker.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.')
-    })
-    });
-    
-    console.log("this is running");
-    hotkeys('ctrl+shift+alt+g', function (event, handler) {
-    console.log('g', event, handler)});
