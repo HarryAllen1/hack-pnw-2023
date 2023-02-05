716f2044927c3977fa6f251e2fc2f15b6aa6669f
lsakunes <senukasl@outlook.com>
2023-02-05 15:01:58 -0800

    keybinds work

---
+++ public/background.js
@@ -0,0 +1,15 @@
+chrome.commands.onCommand.addListener(function (command) {
+    if (command === "keybind1") {
+        console.log("keybind1")
+    }
+    if (command === "keybind2") {
+        console.log("keybind2")
+    }
+    if(command === "keybind3"){
+        console.log("keybind3")
+    }
+    if(command === "keybind4"){
+        console.log("keybind4")
+    }
+});
+
