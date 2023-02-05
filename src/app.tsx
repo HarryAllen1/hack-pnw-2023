ba8f682e518c39a2c44912809be8c3215e2f4528
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-04 18:17:52 -0800

    e

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,6 @@ import { useEffect, useRef, useState } from 'preact/hooks';
 import './app.css';
 import { Command, getCommands, setCommands } from './storage';
 
-const modules = import.meta.glob('./functions/**/*.ts');
-
 export const App: FunctionComponent = () => {
 	const [commands, setCmds] = useState<Command[]>([]);
 	const [creating, setCreating] = useState(false);
@@ -78,22 +76,6 @@ export const App: FunctionComponent = () => {
 									/>
 								</svg>
 							</button>
-							<button class="text-white">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="w-6 h-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-									/>
-								</svg>
-							</button>
 						</div>
 					</div>
 				))}
@@ -111,12 +93,13 @@ export const App: FunctionComponent = () => {
 										code: "console.log('Hello World!');",
 									},
 								]);
-								setCmds(await getCommands());
+								setCmds([...(await getCommands())]);
 								window.open(
 									`editor.html?name=${newInput.current?.value}`,
 									'editor',
 									'popup'
 								);
+								setCreating(false);
 							}}
 						>
 							Create
