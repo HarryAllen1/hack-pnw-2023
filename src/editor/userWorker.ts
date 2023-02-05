907537d8c360cb3652aca1dfca335c70438e4627
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-05 09:57:12 -0800

    MORE STUFF

--- src/editor/userWorker.ts
+++ src/editor/userWorker.ts
@@ -5,7 +5,6 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
-// @ts-ignore
 self.MonacoEnvironment = {
 	getWorker(_: any, label: string) {
 		if (label === 'json') {
