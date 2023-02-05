cac096d7c2360213af6263eb562f0d6568bea051
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-05 12:32:37 -0800

    changes

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,25 +1,14 @@
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import Preact from 'preact';
-import { useEffect, useRef, useState } from 'preact/hooks';
+import { useEffect, useRef } from 'preact/hooks';
 import '../index.css';
+import { EditorView, basicSetup } from 'codemirror';
 import { getCommands, setCommands } from '../storage';
 import styles from './Editor.module.css';
-import functions from './functionTypes.d.ts?raw';
-import './userWorker';
+import { javascript } from '@codemirror/lang-javascript';
 
 const Editor: Preact.FunctionComponent = () => {
-	const [editor, setEditor] =
-		useState<monaco.editor.IStandaloneCodeEditor | null>(null);
-	const monacoEl = useRef(null);
-
-	useEffect(() => {
-		document.querySelectorAll('pre').forEach((el) => {
-			monaco.editor.colorizeElement(el, {
-				theme: 'vs-dark',
-			});
-		});
-	}, []);
-
+	const monacoEl = useRef<HTMLDivElement>(null);
+	let editor: EditorView;
 	useEffect(() => {
 		(async () => {
 			let commands = await getCommands();
@@ -27,33 +16,13 @@ const Editor: Preact.FunctionComponent = () => {
 				(cmd) =>
 					cmd.name === new URLSearchParams(window.location.search).get('name')
 			);
-			if (monacoEl) {
-				setEditor((editor) => {
-					if (editor) return;
-
-					console.log(functions);
-
-					monaco.languages.typescript.javascriptDefaults.addExtraLib(
-						functions,
-						'ts:filename/functions.d.ts'
-					);
-					monaco.editor.createModel(
-						functions,
-						'typescript',
-						monaco.Uri.parse('ts:filename/functions.d.ts')
-					);
-
-					return monaco.editor.create(monacoEl.current!, {
-						value: [thisCommand?.code].join('\n'),
-						language: 'javascript',
-						theme: localStorage.getItem('shortcuts-editor-theme') || 'vs-dark',
-					}) as monaco.editor.IStandaloneCodeEditor | any;
-				});
-			}
+			editor = new EditorView({
+				extensions: [basicSetup, javascript()],
+				parent: monacoEl.current!,
+				doc: thisCommand?.code || '',
+			});
 		})();
-
-		return () => editor?.dispose();
-	}, [monacoEl.current]);
+	}, []);
 
 	return (
 		<div class="flex flex-row">
@@ -66,39 +35,9 @@ const Editor: Preact.FunctionComponent = () => {
 				<p>Example:</p>
 				<pre data-lang="javascript">newTab("https://google.com")</pre>
 
-				<br />
-				<label for="themePicker">Theme</label>
-				<select
-					onChange={(e) => {
-						if ((e.target as HTMLSelectElement).value)
-							monaco.editor.setTheme((e.target as HTMLSelectElement).value);
-						localStorage.setItem(
-							'shortcuts-editor-theme',
-							(e.target as HTMLSelectElement).value
-						);
-					}}
-					name="themePicker"
-					id="themePicker"
-				>
-					<option value=""></option>
-					<option value="vs-dark">VS Dark</option>
-					<option value="vs">VS Light</option>
-				</select>
-				<button
-					onClick={() => {
-						monaco.editor
-							.getEditors()[0]
-							.getAction('editor.action.formatDocument')
-							.run();
-					}}
-				>
-					Format
-				</button>
-				<br />
 				<button
 					onClick={async () => {
-						console.log(monaco.editor.getEditors()[0]);
-						const code = monaco.editor.getEditors()[0]?.getValue();
+						const code = editor.state.doc.toString();
 						if (code) {
 							let commands = (await getCommands()).filter(
 								(cmd) =>
@@ -121,7 +60,10 @@ const Editor: Preact.FunctionComponent = () => {
 					Save
 				</button>
 			</div>
-			<div className={styles.Editor} ref={monacoEl}></div>
+			<div
+				class="h-[100vh] w-[calc(100vw-18rem)] bg-white text-black"
+				ref={monacoEl}
+			></div>
 		</div>
 	);
 };
