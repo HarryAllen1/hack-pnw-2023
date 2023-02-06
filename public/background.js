3795a681ed2813121c844163d96840a41fe62dfb
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-05 16:07:11 -0800

    stuff

--- public/background.js
+++ public/background.js
@@ -1,15 +1,17 @@
 chrome.commands.onCommand.addListener(function (command) {
-    if (command === "keybind1") {
-        console.log("keybind1")
-    }
-    if (command === "keybind2") {
-        console.log("keybind2")
-    }
-    if(command === "keybind3"){
-        console.log("keybind3")
-    }
-    if(command === "keybind4"){
-        console.log("keybind4")
-    }
+	if (command === 'keybind1') {
+		console.log(localStorage.getItem('commands'));
+		eval(JSON.parse(localStorage.getItem('commands'))[0].code);
+	}
+	if (command === 'keybind2') {
+		console.log('keybind2');
+	}
+	if (command === 'keybind3') {
+		console.log('keybind3');
+	}
+	if (command === 'keybind4') {
+		console.log('keybind4');
+	}
 });
 
+onkeydown = console.log;
